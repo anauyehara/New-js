c9b95aac406836bffc223c99194c41ba478b5697
Ana uyehara <ana.uyehara0@gmail.com>
2019-05-01 13:31:08 -0700

    adding buttons

---
+++ js/remote.js
@@ -0,0 +1,6 @@
+var btn = document.getElementsByClassName('button');
+console.log(btn [0]);
+var btnusingselector = document.querySelector ('.button');
+console.log(btnusingselector);
+btn[0].style.padding = '10px';
+btn[0].style.fontsize = '22px';
