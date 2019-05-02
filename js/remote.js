d68db08cfbcd1c852c32b870ff24aa84512ad9ed
Ana uyehara <ana.uyehara0@gmail.com>
2019-05-02 13:30:01 -0700

    fixed buttons

--- js/remote.js
+++ js/remote.js
@@ -4,3 +4,9 @@ var btnusingselector = document.querySelector ('.button');
 console.log(btnusingselector);
 btn[0].style.padding = '10px';
 btn[0].style.fontsize = '22px';
+
+
+body{
+ background-Image: url(beach.jpg);
+ background-position: 'center';
+}
